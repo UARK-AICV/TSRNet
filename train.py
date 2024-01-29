23a82fe8be7cfad39ea958197a71cad9e0e97cef
Nhat-Tan Bui <60350737+tanbuinhat@users.noreply.github.com>
2024-01-29 15:12:13 -0600

    Update train.py

--- train.py
+++ train.py
@@ -228,7 +228,7 @@ if __name__ == '__main__':
     parser.add_argument("--gpu", type=str, default="2")
     parser.add_argument("--spec", default=True)
     parser.add_argument("--pth_path", type=str, default=None)
-    parser.add_argument("--mask_loss", default=True)
+    parser.add_argument("--mask_loss", default=True)  #Peak-based Error
 
     args = parser.parse_args()
     
